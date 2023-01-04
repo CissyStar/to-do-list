26ec0c7d1c4b67f41188652d611c214b8e60f219
CissyStar <102735540+CissyStar@users.noreply.github.com>
2023-01-04 15:36:04 +1100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import "./App.css";
 import ToDoList from "./pages/TodoList";
-// import Playlist from "./pages/Playlist";
-// import data from "./data";
+
 import { useState } from "react";
 
 function App() {
-  // practice for playlist component
-  // const [playList, setPlayList] = useState(data);
   
   return (
     <div className="App">
       <ToDoList /> 
-      {/* <Playlist playList={playList} /> */}
     </div>
   );
 }
